0a58d9ac4bc4e27a47a67ac4d2487656d2e91cac
Thomoop <51370363+Thomoop@users.noreply.github.com>
2024-07-27 17:31:44 -0400

    updates

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const axios = require('axios');
 const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-const GODADDY_KEY = process.env.GODADDY_KEY;
-const GODADDY_SECRET = process.env.GODADDY_SECRET;
-const DOMAIN = process.env.CLIENT_URL;
-const RECORD_NAMES = ['@', '*'];
+const DOMAIN = process.env.DOMAIN;
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASS = process.env.EMAIL_PASS;
+const EMAIL_TO = process.env.EMAIL_TO;
 
 let currentIp = '';
 
 async function getPublicIp() {
+    console.log('Obteniendo IP pública...');
     const services = [
         'https://api64.ipify.org?format=json',
         'https://httpbin.org/ip',
@@ -17,60 +20,79 @@ async function getPublicIp() {
 
     for (const service of services) {
         try {
+            console.log(`Intentando servicio: ${service}`);
             const response = await axios.get(service);
             if (service.includes('ipify') || service.includes('httpbin')) {
+                console.log(`IP de ${service}: ${response.data.ip || response.data.origin.trim()}`);
                 return response.data.ip || response.data.origin.trim();
             } else {
+                console.log(`IP de ${service}: ${response.data.trim()}`);
                 return response.data.trim();
             }
         } catch (error) {
-            console.warn(`Error getting public IP from ${service}:`, error.message);
+            console.warn(`Error al obtener la IP pública de ${service}:`, error.message);
         }
     }
 
-    console.error('All services failed to get public IP.');
+    console.error('Todos los servicios fallaron al obtener la IP pública.');
     return null;
 }
 
-async function updateGodaddyDns(ip) {
-    const config = {
-        headers: {
-            Authorization: `sso-key ${GODADDY_KEY}:${GODADDY_SECRET}`,
-            'Content-Type': 'application/json'
+async function sendEmail(newIp) {
+    let transporter = nodemailer.createTransport({
+        host: "smtpout.secureserver.net",
+        port: 465,
+        secure: true,
+        auth: {
+            user: EMAIL_USER,
+            pass: EMAIL_PASS
         }
+    });
+
+    let mailOptions = {
+        from: EMAIL_USER,
+        to: EMAIL_TO,
+        subject: 'Cambio de Dirección IP',
+        text: `La dirección IP ha cambiado a ${newIp}.
+
+Por favor, sigue estos pasos para actualizar la configuración DNS:
+
+1. Ve a la página de administración DNS de GoDaddy: https://dcc.godaddy.com/control/dnsmanagement?domainName=${DOMAIN}
+2. Inicia sesión con tus credenciales de GoDaddy.
+3. Encuentra los registros DNS para tu dominio.
+4. Actualiza la dirección IP para los registros '@' y '*' a la nueva dirección IP: ${newIp}.
+5. Guarda los cambios.
+`
     };
-
-    for (const RECORD_NAME of RECORD_NAMES) {
-        const url = `https://api.godaddy.com/v1/domains/${DOMAIN}/records/A/${RECORD_NAME}`;
-        const data = [{ data: ip }];
-
-        try {
-            await axios.put(url, data, config);
-            console.log(`DNS record for ${RECORD_NAME} updated to ${ip}`);
-        } catch (error) {
-            console.error(`Error updating DNS record for ${RECORD_NAME}:`, error.response ? error.response.data : error.message);
+    console.log('Enviando correo...');
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log('Error al enviar el correo:', error);
+        } else {
+            console.log('Correo enviado:', info.response);
         }
-    }
+    })
 }
 
 async function checkAndUpdateIp() {
+    console.log('Comprobando y actualizando la IP...');
     const newIp = await getPublicIp();
-    console.log(`Current IP: ${currentIp}`);
-    console.log(`New IP: ${newIp}`);
+    console.log(`IP actual: ${currentIp}`);
+    console.log(`Nueva IP: ${newIp}`);
     if (newIp && newIp !== currentIp) {
-        await updateGodaddyDns(newIp);
-        console.log(`IP changed from ${currentIp} to ${newIp}`);
+        await sendEmail(newIp);
+        console.log(`La IP ha cambiado de ${currentIp} a ${newIp}`);
         currentIp = newIp;
     } else {
-        console.log('IP has not changed');
+        console.log('La IP no ha cambiado');
     }
 }
 
-// Run the function every 5 minutes
+// Ejecutar la función cada 5 minutos
 cron.schedule('*/5 * * * *', () => {
-    console.log('Checking IP...');
+    console.log('Comprobando IP...');
     checkAndUpdateIp();
 });
 
-// Initial check
+// Comprobación inicial
 checkAndUpdateIp();
